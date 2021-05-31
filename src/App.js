d05b248085c11ae08949f556e587f3c39320af82
Archita Jain <jainarchita12@gmail.com>
2021-05-31 19:13:15 +0530

    Rendering the contacts

--- src/App.js
+++ src/App.js
@@ -4,16 +4,42 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Archita Jain",
+        phone: "9999999999"
+      },
+      {
+        id: 2,
+        name: "Leslie Knope",
+        phone: "9999999998"
+      },
+      {
+        id: 3,
+        name: "Nick Miller",
+        phone: "9999999997"
+      }
+    ];
+
     return (
       <div>
         <Header/>
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
-
           <div className="grid-container heading-container">
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+              </div>
+            })
+          }
         </div>
       </div>
     );
